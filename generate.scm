3fdeaf12d2f7eb2bba31765d6eddcf7b37538450
Vasyl Bodnar <vasyl_bodnar@outlook.com>
2025-12-26 18:59:06 -0500

    Added a generation script

---
+++ generate.scm
@@ -0,0 +1,27 @@
+(use-modules (scheme base))
+(use-modules (rnrs bytevectors))
+
+(define (map-name op val)
+  (case op
+    ((push) (ash val 3))
+    ((pop) (logior 1 (ash val 3)))
+    ((add) (logior 2 (ash val 3)))
+    ((rep) (logior 3 (ash val 3)))
+    ((ext) (logior 4 (ash val 3)))
+    ((cop) (logior 5 (ash val 3)))
+    ((load) (logior 6 (ash val 3)))
+    ((store) (logior 7 (ash val 3)))
+    (else (display "Not an op"))))
+
+(define prog
+  '((push 1)
+    (add 1)
+    (store 0)))
+
+(define out (u8-list->bytevector (map (lambda (x) (map-name (car x) (cadr x))) prog)))
+
+(call-with-output-file "out.byt"
+  (lambda (port)
+    (write-bytevector out port)))
+
+(display out)
